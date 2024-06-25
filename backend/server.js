5ade223ba0a0170ad255be8555bfe43f5a8920a2
amitsingh771087 <amitsingh77108792@gmail.com>
2024-06-25 17:06:25 +0530

    auth controller added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
-import express from "express"
-import authRoutes from "./routes/auth.routes.js"
+import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/conectMongoDB.js";
+
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-console.log(process.env.MONGO_URI)
+
+
+app.use(express.json());//to parse request.body
+app.use(express.urlencoded({extended:true })) //to parse from data
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+
+
 
 
 app.listen(PORT, ()=>{
